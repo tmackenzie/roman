3aedcf4ba260c6012c3cbd5e0aeee23206483d55
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2012-08-25 17:54:09 -0500

    to roman faster version.

--- roman/calculator.py
+++ roman/calculator.py
@@ -1,3 +1,4 @@
+import math
 
 def int_to_roman(input):
     ''' 
@@ -9,31 +10,21 @@ def int_to_roman(input):
     if not 0 < input < 4000:
        raise ValueError, "input must be between 1 and 3999"   
 
-    romans = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
-    ints = [[ 1000,],
-            [ 900, 500, 400, 100,]
-            [ 90, 50, 40, 10, 9, 5, 4, 1,]]
-
-    ints_to_romans = { 1000: 'M',
-                       900: 'CM',
-                       500: 'D',
-                       400: 'CD',
-                       100: 'C',
-                       90: 'XC',
-                       50: 'L',
-                       40: 'XL',
-                       10: 'X',
-                       9: 'IX',
-                       5: 'V',
-                       4: 'IV',
-                       1: 'I'}
+    ints_to_romans = [[ (9,'IX'), (5,'V'), (4, 'IV'), (1,'I')],
+                      [ (90, 'XC'), (50, 'L'), (40,'XL'), (10,'X')],
+                      [ (900, 'CM'), (500,'D'),(400,'CD'),( 100,'C')],
+                      [ (1000, 'M')]]
 
     result = []
 
-    for i in range(len(romans)):
-        int_index = input / ints[i]
-        result.append(romans[i] * int_index)
-        input -= ints[i] * int_index
+    index_lookup = int(math.log10(input))
+    while input != 0:
+        for digit, roman in ints_to_romans[index_lookup]:
+            int_index = input / digit
+            result.append(roman * int_index)
+            input -= digit * int_index
+
+        index_lookup-=1
 
     return ''.join(result)
 
