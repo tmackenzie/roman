c3db97f1357c5345c9d0e696b8ea155b7532117f
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2012-08-25 17:04:14 -0500

    starting on speed..

--- roman/calculator.py
+++ roman/calculator.py
@@ -10,7 +10,23 @@ def int_to_roman(input):
        raise ValueError, "input must be between 1 and 3999"   
 
     romans = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
-    ints = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    ints = [[ 1000,],
+            [ 900, 500, 400, 100,]
+            [ 90, 50, 40, 10, 9, 5, 4, 1,]]
+
+    ints_to_romans = { 1000: 'M',
+                       900: 'CM',
+                       500: 'D',
+                       400: 'CD',
+                       100: 'C',
+                       90: 'XC',
+                       50: 'L',
+                       40: 'XL',
+                       10: 'X',
+                       9: 'IX',
+                       5: 'V',
+                       4: 'IV',
+                       1: 'I'}
 
     result = []
 
