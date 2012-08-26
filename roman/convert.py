c96e06d8ae76d68cfae1b9100165ec9a34dff1bf
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2012-08-26 15:30:48 -0500

    pep8 compliant.

--- roman/convert.py
+++ roman/convert.py
@@ -86,6 +86,7 @@ def int_to_roman(input):
 
     return ''.join(result)
 
+
 def roman_to_int(input):
     '''
         Given a string that represents a roman numeral, then,
@@ -108,38 +109,38 @@ def roman_to_int(input):
     # parse input..
     thousands, hundreds, tens, ones = roman_group_re.match(input).groups()
 
-    # should be pretty simple, all I need is a dict to match the vars to their ints.
+    # should be pretty simple, need a dict to match the romans to ints
     roman_to_int = {
-         "I": 1,
-         "II": 2,
-         "III": 3,
-         "IV": 4,
-         "V": 5,
-         "VI": 6,
-         "VII": 7,
-         "VIII": 8,
-         "IX": 9,
-         "X": 10,
-         "XX": 20,
-         "XXX": 30,
-         "XL": 40,
-         "L": 50,
-         "LX": 60,
-         "LXX": 70,
-         "LXXX": 80,
-         "XC": 90,
-         "C": 100,
-         "CC": 200,
-         "CCC": 300,
-         "CD": 400,
-         "D": 500,
-         "DC": 600,
-         "DCC": 700,
-         "DCCC": 800,
-         "CM": 900,
-         "M": 1000,
-         "MM": 2000,
-         "MMM": 3000}
+        "I": 1,
+        "II": 2,
+        "III": 3,
+        "IV": 4,
+        "V": 5,
+        "VI": 6,
+        "VII": 7,
+        "VIII": 8,
+        "IX": 9,
+        "X": 10,
+        "XX": 20,
+        "XXX": 30,
+        "XL": 40,
+        "L": 50,
+        "LX": 60,
+        "LXX": 70,
+        "LXXX": 80,
+        "XC": 90,
+        "C": 100,
+        "CC": 200,
+        "CCC": 300,
+        "CD": 400,
+        "D": 500,
+        "DC": 600,
+        "DCC": 700,
+        "DCCC": 800,
+        "CM": 900,
+        "M": 1000,
+        "MM": 2000,
+        "MMM": 3000}
 
     result = 0
 
