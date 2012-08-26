876a53ad58f0b4b19bd77853cd19c224073d8388
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2012-08-26 15:41:27 -0500

    pep8 compliant pulled over tests from master

--- roman/convert.py
+++ roman/convert.py
@@ -1,20 +1,22 @@
 '''
-    The following is taken from the Python Cookbook, by Alex Martelli, David Ascher
-    http://books.google.com/books?id=yhfdQgq8JF4C
+    The following is taken from the Python Cookbook,
+    by Alex Martelli, David Ascher
 
+    http://books.google.com/books?id=yhfdQgq8JF4C
 '''
 
 
 def int_to_roman(input):
-    ''' 
+    '''
        given and integer, input, that is greater than 0 and less than, 4000
        return its modern roman numeral represenation
 
        worst/best case.
-       O(1) - b/c no matter what the input is, 
-        - it will always execute the same methods, comparators and assignments.   
+       O(1) - b/c no matter what the input is,
+        - it will always execute the same methods, comparators and assignments.
 
-       13 * ( 3 operations, 2 reads, 2 writes) + 2 method calls + 2 comparators.
+       13 * ( 3 operations, 2 reads, 2 writes)
+       + 2 method calls + 2 comparators.
 
        performs:
         - 2 method calls.
@@ -27,9 +29,9 @@ def int_to_roman(input):
     '''
 
     if not 0 < input < 4000:
-       raise ValueError, "input must be between 1 and 3999"   
+        raise ValueError("input must be between 1 and 3999")
 
-    romans = ( 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
+    romans = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
     ints = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
 
     result = []
@@ -37,28 +39,30 @@ def int_to_roman(input):
     for i in range(len(romans)):
         count = input / ints[i]
         result.append(romans[i] * count)
-        input -= ints[i] * count 
+        input -= ints[i] * count
 
     return ''.join(result)
 
+
 def roman_to_int(input):
-    ''' 
-        Given a string that represents a roman numeral, return its integer value
     '''
-    
+        Given a string that represents a roman numeral, then,
+        return its integer value
+    '''
+
     input = input.upper()
-    nums = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+    nums = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     sum = 0
     for i in range(len(input)):
         try:
             value = nums[input[i]]
 
-            # If the next place holds a larger number, this value is negative.            
-            if i+1 < len(input) and nums[input[i+1]] > value:
+            # If the next place holds a larger number, this value is negative.
+            if i + 1 < len(input) and nums[input[i + 1]] > value:
                 sum -= value
             else:
                 sum += value
         except KeyError:
-            raise ValueError, 'input is not a valid Roman numeral: %s' % input
+            raise ValueError('input is not a valid Roman numeral: %s' % input)
 
-    return sum 
+    return sum
