6db16a95838bf84fb219c269dd5fdea23ccf2ed2
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2012-08-26 20:48:12 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='roman',
+      version='1.3',
+      description='Integer and Roman Numeral Converter',
+      author='Tom Mackenzie',
+      author_email='tom.s.mackenzie@gmail.com',
+      url='https://github.com/tmackenzie/roman',
+    )
